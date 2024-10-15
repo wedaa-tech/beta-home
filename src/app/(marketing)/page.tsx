c368b4ff56b39168dbf639f79533eeb958673b26
ShyamCMIT <shyamsai.dogga@xebia.com>
2024-10-15 22:45:21 +0530

    Fixed home

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -76,7 +76,7 @@ const HomePage = () => {
                             <div className="absolute top-1/2 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
                             <div className="-m-2 rounded-xl p-2 ring-1 ring-inset ring-foreground/20 lg:-m-4 lg:rounded-2xl bg-opacity-50 backdrop-blur-3xl">
                                 <Image
-                                    src="../assets/dashboard1.svg"
+                                    src="./assets/dashboard1.svg"
                                     alt="banner image"
                                     width={1200}
                                     height={1000}
